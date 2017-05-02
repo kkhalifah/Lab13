33eee5b3772bcb7095002315e47b1d636a4aa07f
kkhalifah <karen.khalifah@gmail.com>
2017-05-02 18:49:11 -0400

    working

--- app.js
+++ app.js
@@ -5,16 +5,9 @@
 
 app.controller('ourController', function($scope){
 
-  var words=    [{text: 'fork ', cls:''},
-              {text: 'puppies ', cls:'' },
-              {text: 'flowers ', cls:'' } ];
+  var words=    ['fork ' ,'puppies ','flowers '];
 
-    var multipleWords =  [{text: 'cats are weird ', cls: ''},
-                          {text: 'I like grass ', cls: ''},
-                          {text: 'why go there ', cls: ''},
-                          {text: 'rocks are soft ', cls: ''},
-                          {text: 'girls are gross ', cls: ''},
-                          {text: 'my cup is broken ', cls: ''}];
+    var multipleWords =  ['cats are weird ', 'I like grass ','why go there ', 'rocks are soft ','girls are gross ','my cup is broken '];
 
     var classColor = ['one', 'two', 'three', 'four', 'five', 'six'];
 
@@ -23,7 +16,8 @@ app.controller('ourController', function($scope){
 // this is the function to add Oneword with no class(decoration)
    $scope.addOneWord = function(){
      var obj = words[Math.floor(Math.random()*words.length)];
-     $scope.newWords.push(obj);
+     var newobj = {name: obj};
+     $scope.newWords.push(newobj);
     // console.log(obj);
    };
 
@@ -31,23 +25,23 @@ app.controller('ourController', function($scope){
    $scope.addWordDecoration = function (){
      var obj = words[Math.floor(Math.random()*words.length)];
      var style = classColor[Math.floor(Math.random()*classColor.length)];
-     obj.cls = style;
-     $scope.newWords.push(obj);
+
+     $scope.newWords.push({name: obj, cls: style});
      //console.log(obj);
+     console.log(words);
    };
 
    //function to add  more than one word-no decoration
    $scope.addMoreWord = function (){
      var obj = multipleWords[Math.floor(Math.random()*multipleWords.length)];
-     $scope.newWords.push(obj);
+     $scope.newWords.push({name: obj});
    };
 
    //function to add  more than one word-with decoration
    $scope.addMoreWordDecoration = function (){
      var obj = multipleWords[Math.floor(Math.random()*multipleWords.length)];
      var style = classColor[Math.floor(Math.random()*classColor.length)];
-     obj.cls = style;
-     $scope.newWords.push(obj);
+     $scope.newWords.push({name:obj, cls: style});
    };
 
 
